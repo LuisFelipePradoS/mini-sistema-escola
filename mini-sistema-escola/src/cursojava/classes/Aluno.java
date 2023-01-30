c7b12b281eb40029874cd677f89337a334094ca0
Luis Felipe Prado da Silva <luisf.profissional.acco@outlook.com>
2023-01-30 13:34:44 -0300

    Implementação da classe Aluno

--- mini-sistema-escola/src/cursojava/classes/Aluno.java
+++ mini-sistema-escola/src/cursojava/classes/Aluno.java
@@ -1,5 +1,88 @@
 package cursojava.classes;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Aluno {
 
+	private String nome;
+	private int idade;
+	private String cpf;
+	private String rg;
+	private String endereco;
+	private String telefone;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (rg == null) {
+			if (other.rg != null)
+				return false;
+		} else if (!rg.equals(other.rg))
+			return false;
+		return true;
+	}
+	
+	//toString() vai aqui
+	
+	//calculaMedia() vai aqui
+	
+	//resultado() vai aqui
 }
