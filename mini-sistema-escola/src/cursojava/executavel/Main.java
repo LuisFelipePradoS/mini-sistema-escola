acc9c9cb704df33aedf97c3e2649d998f49c1368
Luis Felipe Prado da Silva <luisf.profissional.acco@outlook.com>
2023-01-30 14:05:15 -0300

    Implementação da validação de acesso

--- mini-sistema-escola/src/cursojava/executavel/Main.java
+++ mini-sistema-escola/src/cursojava/executavel/Main.java
@@ -1,10 +1,28 @@
 package cursojava.executavel;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+
+import cursojava.classes.*;
+import cursojava.constantes.StatusAluno;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String user = JOptionPane.showInputDialog("Digite seu usuário");
+		String password = JOptionPane.showInputDialog("Digite sua senha");
+		
+		if (user.equals("admin") && password.equals("admin")) {
+			
+			//Códigos das rotinas
+			
+		} else {
+			
+			JOptionPane.showMessageDialog(null, "Usuário ou Senha incorretos!");
+		}
 	}
 
 }
